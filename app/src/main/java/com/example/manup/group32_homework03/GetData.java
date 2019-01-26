c598d53873575bc70e6af5e50ee8d5f2be4d3af7
Priyanka <manupriyanka1223@github.com>
2019-01-26 16:24:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/manup/group32_homework03/GetData.java
@@ -0,0 +1,26 @@
+package com.example.manup.group32_homework03;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by manup on 2/16/2018.
+ */
+
+public class GetData extends AsyncTask<String,Void,ArrayList<String>>{
+    @Override
+    protected ArrayList<String> doInBackground(String... strings) {
+        return null;
+    }
+
+
+}
+
